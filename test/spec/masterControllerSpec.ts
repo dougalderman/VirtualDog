0c3d042ecd4d6ebdc9fab83a518d06d446403ae9
Doug Alderman <agnadcp-d1@yahoo.com>
2016-12-07 11:21:12 -0700

    first test

---
+++ test/spec/masterControllerSpec.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../typings/index.d.ts" />
+describe('my first test', () => {
+    it('my first expectation', () => {
+        expect(true).toBeTruthy('What? Why was true not truthy?');
+    });
+});
